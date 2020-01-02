2420f9d4af538ad51e3b65cb1b682eabd82a0037
B Severino <biancasev@gmail.com>
2020-01-02 17:40:27 -0500

    Added types data

--- mbformillennials/src/data/types.js
+++ mbformillennials/src/data/types.js
@@ -2,5 +2,65 @@ export const types = [
     {
         type: 'INTJ',
         name: 'Architect'
+    },
+    {
+        type: 'INTP',
+        name: 'Logician'
+    },
+    {
+        type: 'ENTJ',
+        name: 'Commander'
+    },
+    {
+        type: 'ENTP',
+        name: 'Debater'
+    },
+    {
+        type: 'INFJ',
+        name: 'Advocate'
+    },
+    {
+        type: 'INFP',
+        name: 'Mediator'
+    },
+    {
+        type: 'ENFJ',
+        name: 'Protagonist'
+    },
+    {
+        type: 'ENFP',
+        name: 'Campaigner'
+    },
+    {
+        type: 'ISTJ',
+        name: 'Logistician'
+    },
+    {
+        type: 'ISFJ',
+        name: 'Defender'
+    },
+    {
+        type: 'ESTJ',
+        name: 'Executive'
+    },
+    {
+        type: 'ESFJ',
+        name: 'Consul'
+    },
+    {
+        type: 'ISTP',
+        name: 'Virtuoso'
+    },
+    {
+        type: 'ISFP',
+        name: 'Adventurer'
+    },
+    {
+        type: 'ESTP',
+        name: 'Entrepreneur'
+    },
+    {
+        type: 'ESFP',
+        name: 'Entertainer'
     }
 ];
