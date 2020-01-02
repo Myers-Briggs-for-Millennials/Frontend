f12d15c57288d20b5a957e03fcb3a5edd57bd52d
Alexander Sierra <alexandercsierra@gmail.com>
2020-01-02 14:26:10 -0500

    working with dummy data

--- mbformillennials/src/App.js
+++ mbformillennials/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Question from './components/Question'
+import questionSet from './components/data'
 
 
 function App() {
@@ -14,16 +15,33 @@ function App() {
   const [thinking, setThinking] = useState(0);
   const [judging, setJudging] = useState(0);
   const [perceiving, setPerceiving] = useState(0);
+  const [indexes, setIndexes] = useState([0,1,2,3]);
+  const [allQuestions, setAllQuestions] = useState(questionSet);
+  const [currentQuestions, setCurrentQuestions] = useState(questionSet);
 
 
-  
+
+  //make copy of array
+  //push new questions
+  //state for array
+
+
+
 
 
   return (
     <div className="App">
       <h1>Myers Briggs For Millenials</h1>
+      {currentQuestions.map(quest =>{
+        return <Question setSlider={setSlider} slider={slider} question={quest}/>
+      })}
       
-      <Question setSlider={setSlider} slider={slider}/>
+      <button onClick={e => {
+        e.preventDefault();
+        indexes.map(index => {
+          return questionSet[index];
+        })
+      }}>Next Page</button>
     </div>
   );
 }
