0d87a4d091266ccf03c37943b93c34c9bcb5de88
CodingCCarpenter <christineassists@aol.com>
2020-01-03 11:49:29 -0600

    import images and add to type objects

--- mbformillennials/src/data/types.js
+++ mbformillennials/src/data/types.js
@@ -7,101 +7,118 @@ import stonks from '../img/memes/stonks.png';
 import surprisedPikachu from '../img/memes/surprised-pikachu.png';
 import womanYellingAtCat from '../img/memes/woman-yelling-at-cat.jpg';
 
+import ISTJ from '../img/memes/ISTJ.png';
+import ISFJ from '../img/memes/ISFJ.png';
+import INFJ from '../img/memes/INFJ.png';
+import INTJ from '../img/memes/INTJ.png';
+import ISTP from '../img/memes/ISTP.png';
+import ISFP from '../img/memes/ISFP.png';
+import INFP from '../img/memes/INFP.png';
+import INTP from '../img/memes/INTP.png';
+import ESTP from '../img/memes/ESTP.png';
+import ESFP from '../img/memes/ESFP.png';
+import ENFP from '../img/memes/ENFP.png';
+import ENTP from '../img/memes/ENTP.png';
+import ESTJ from '../img/memes/ESTJ.png';
+import ESFJ from '../img/memes/ESFJ.png';
+import ENFJ from '../img/memes/ENFJ.png';
+import ENTJ from '../img/memes/ENTJ.png';
+
 export const types = [
     {
         type: 'INTJ',
         name: 'Architect',
         description: `INTJ stands for Introverted iNtuitive Thinking Judging. This personality type is withdrawn, intellectual, reflective, unconventional, abstract, insightful and methodical. These people are typically hard to read and tend to come across as dispassionate, unemotional, quirky, yet very self-controlled. Their most treasured pursuit is the one for knowledge, for the fundamental truth behind appearances and INTJs have a natural flair for seeking, discerning and understanding the very essence of things. While on the outside, they often appear to be logical, cautious, rational and skeptical individuals, their inner life of the mind is rich beyond belief with symbols, meaning and innovative visions of the future. Although often brilliant and ahead of their time, they are sometimes perceived by their peers as being rather strange, arrogant and distant.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [INTJ]
     },
     {
         type: 'INTP',
         name: 'Logician',
         description: `INTP stands for Introverted iNtuitive Thinking Perceiving. This personality type is known for being intellectual, abstract, logical, reserved, detached, random and skeptical. Such people are usually unconventional and highly theoretical, they like playing with ideas and coming up with ingenious thought systems and explanations for the events they observe, although they aren't too likely to share their discoveries, as they're quite socially withdrawn and secretive. INTPs are also objective, spontaneous, adaptable and highly independent; they often question authority, openly disregard social conventions and irrational customs and prefer to think for themselves, which can lead to an original, avant-garde vision of things, but also to being perceived as arrogant, cold and eccentric.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [INTP]
     },
     {
         type: 'ENTJ',
         name: 'Commander',
         description: `ENTJ stands for Extroverted iNtuitive Thinking Judging. People with this personality type are generally strong, expansive, domineering, decisive, clear-cut and logical. They are very pragmatic and extraordinary strategic thinkers who have both the vision and the ability to make their vision come true. ENTJs are natural leaders, goal-oriented, innovative, determined, powerful, they like to make things happen and don't have much patience with incompetence, inefficiency, hurt feelings or any other things they believe might slow them down. They are good at directing teams and can make wonderful leaders, although at times they can be perceived by others as somewhat insensitive, argumentative and domineering.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ENTJ]
     },
     {
         type: 'ENTP',
         name: 'Debater',
         description: `ENTP stands for Extroverted iNtuitive Thinking Perceiving. People with this personality type are usually optimistic, outgoing, inventive, logical, playful, spontaneous and humorous. They are quick-minded, rational and flexible and they find it easy to improvise and come up with a multitude of ideas and solutions, which can make them ingenious problem solvers and even pioneers in their fields of interest. Their adventurousness and openness to new experiences, combined with their sociable, fun-loving nature often make them widely popular and well-liked by many of their peers. But despite their cheerfulness and carefree attitude, ENTPs often harbor a sharp, strategic mind and they are perfectly capable of detaching from their feelings in order to pragmatically reach their objectives. This very aspect can make some people regard them as unpredictable, insensitive and egotistical.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ENTP]
     },
     {
         type: 'INFJ',
         name: 'Advocate',
         description: `INFJ stands for Introverted iNtuitive Feeling Judging. This personality type is generally very private, empathetic, wise, thoughtful, profound, romantic and complex. They have rich inner worlds, fostered by their wide imagination, which reflect their love for abstract symbols and spiritual meaning. Their insightful understanding of the human nature makes them talented at exploring and capturing the subtle emotional truths of the human experience (often through beautiful artistic creations). INFJs are sympathetic, humanitarian individuals, who strive for harmony, cooperation and unity among people, yet their reserved, introspective nature eventually urges them to seek privacy in order to sort out their feelings and ideas. Although wise and insightful, their emotional complexity can sometimes make them appear as overly sensitive, moody and strange.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [INFJ]
     },
     {
         type: 'INFP',
         name: 'Mediator',
         description: `INFP stands for Introverted iNtuitive Feeling Perceiving. People who have this personality type are generally reserved, tolerant, idealistic, sympathetic, insightful, romantic, natural and unplanned. They are avidly searching for the inner beauty of nature and people, the harmonious balance of life and the true value of everything around them. Intensely imaginative, intuitive and open-minded, they easily perceive the human potential that's hidden behind appearances and become inspired by it. INFPs are empathetic listeners, genuinely interested in what other people have to say, but at the same time their need for privacy and introspection can make them retreat from social contact in order to process their feelings undisturbed. This emotional sensitivity can sometimes make them appear as unsociable, moody and illogical.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [INFP]
     },
     {
         type: 'ENFJ',
         name: 'Protagonist',
         description: `ENFJ stands for Extroverted iNtuitive Feeling Judging. Individuals with this personality type are usually compassionate, understanding, insightful, open-minded, generous and altruistic, but also organized and driven, on a professional level. They are both kind and firm, are amazing public speakers and enjoy being among people, helping, understanding and guiding them through their troubles. ENFJs make exceptional friends, as they gregarious, generous, cultivated and usually have a penetrating perception of the emotional needs of others, which they consequently feel the urge to meet. Although empathetic and affectionate, they can at times become quite demanding, controlling and excessively image-conscious.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ENFJ]
     },
     {
         type: 'ENFP',
         name: 'Campaigner',
         description: `ENFP stands for Extroverted iNtuitive Feeling Perceiving. This personality type is known for being spontaneous, optimistic, creative, expansive, playful and very charismatic. They are curious people, with a great need for variety, change, adventure and emotional engagement. Two of their best qualities are their wide imagination and their versatility - this type is a quick, abstract learner, excels at brain-storming and loves having causes to believe in and work for. Naturally, ENFPs are sociable humanists who love helping and understand other people. They are warm, very friendly and generous with their time and energy, but they do have a tendency to spread themselves too thin and find it difficult to keep all their promises they make or complete all the projects they take on.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ENFP]
     },
     {
         type: 'ISTJ',
         name: 'Logistician',
         description: `ISTJ stands for Introverted Sensing Thinking Judging. This personality type is stable, precautious, traditional, detail-oriented, practical and orderly. They are very conscientious people that can be relied upon to do a thorough job, as they take their duties very seriously and prefer things to be structured and logically ordered. Pillars of their society, ISTJs excel at working with detail (they do not mind routine as much as other types), they are precise, meticulous and very down-to-earth. They respect traditions, the longstanding values of their society, the precious lessons of history and also the hierarchical structure of people and things. Although honest and hard-working, they may be perceived by others as a bit old-fashioned, tedious and frugal.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ISTJ]
     },
     {
         type: 'ISFJ',
         name: 'Defender',
         description: `ISFJ stands for Introverted Sensing Feeling Judging. People who have this personality type are generally kind, empathetic, traditional, shy, caring, practical and very loyal. They love helping and assisting others around them and often assume a nurturing role in their family and society, as they make wonderful caregivers and mother-figures. Often self-effacing and unpretentious, they in fact have very strong moral convictions and a deep-seated sense of their social and human duty and purpose. ISFJs are very affectionate, committed and dedicated to their loved ones and they are often willing to sacrifice their own wishes and needs in order meet those of others, which may eventually end up causing them a fair amount of emotional suffering and frustration, in the long run.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ISFJ]
     },
     {
         type: 'ESTJ',
         name: 'Executive',
         description: `ESTJ stands for Extroverted Sensing Thinking Judging. Individuals with this personality type are realistic, practical, organized, hard-working, conventional, responsible, outgoing and assertive. They are very rational people who trust and follow clear logical principles and tend to approach every situation in an objective and impartial manner. Highly realistic and driven, their energy is often invested in pursuits that have a practical application and a tangible result. Their stable, industrious personality is based on a deep-seated sense of duty towards the social system they are a part of and the established rules attached to it. Although earnest, loyal and respectful to their society, ESTJs are also strong, decisive people, who can lead and organize others confidently. As a result, they can sometimes be perceived as quite rigid, tactless and domineering by those around them.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ESTJ]
     },
     {
         type: 'ESFJ',
         name: 'Consul',
         description: `ESFJ stands for Extroverted Sensing Feeling Judging. People with this personality type are kind, polite, helpful, altruistic, affectionate, traditional and friendly. They need to feel that they're a useful and appreciated part of their community; therefore they will embrace and champion the social conventions and traditions that exist in their society. Orderly, neat and well-behaved, they are always careful to appropriately fulfill their social role, as dictated by the nature of the relationships involved. ESFJs are often pleasant, gracious, sympathetic people, who love to help others in practical and tangible ways, making sure everyone is fitting in and relationships are peaceful and harmonious within their family or group. Their acute need for praise and gratitude, however, may occasionally cause others to see them as clingy, manipulative and pushy.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ESFJ]
     },
     {
         type: 'ISTP',
         name: 'Virtuoso',
         description: `ISTP stands for Introverted Sensing Thinking Perceiving. People with this personality type are typically independent, individualistic, practical, handy, audacious and very realistic. They are adaptable and casual, trust their own experience and are very skillful at coordinating their minds with their bodies, which makes them wonderful at handling different tools or working with equipment and machinery. Their freedom and autonomy is highly important to them so they prefer working independently and keeping demands on their person to a minimum. ISTPs are also very rational, straightforward people who usually keep to themselves and dislike social pressure, things that may actually cause them difficulties in their relationships, occasionally being perceived as inconsiderate, selfish and unreliable.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ISTP]
     },
     {
         type: 'ISFP',
         name: 'Adventurer',
         description: `ISFP stands for Introverted Sensing Feeling Perceiving. People with this personality type are known for being gentle, compassionate, uncomplicated, spontaneous, natural, peaceful and modest. They highly value their personal freedom and need plenty of private time and space, in which to follow their calling, very often represented by nature and artistic activities. Kind, generous and sensitive, ISFPs often have a developed emotional intelligence and are gifted at correctly reading the body language and unexpressed feelings of those around them. Their warm and empathetic nature, as well as their need for harmony and serenity, will drive them to help those in need or in suffering, usually in concrete and tangible ways. Despite their childlike joy and playfulness, they can be regarded by some as hesitant, soft and naive.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ISFP]
     },
     {
         type: 'ESTP',
         name: 'Entrepreneur',
         description: `ESTP stands for Extroverted Sensing Thinking Perceiving. This personality type is known for being outgoing, self-confident, entertaining, rational, pragmatic, and matter-of-fact. They like having an active lifestyle, countless friends and being up-to-date with everything that's going on in their world, as they want to always keep up with the latest trends. Fun, adventurous and charismatic, they are also highly competitive and enterprising people with well-developed tactic skills, which they often employ to gain advantage in immediate situations. ESTPs prefer to live in the moment and rarely make long-term plans - they're spontaneous, action-oriented and quite hedonistic, with a flair for spotting opportunities and making the most out of every situation. This approach, however, can make some people regard them as irresponsible, superficial and inconsiderate.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ESTP]
     },
     {
         type: 'ESFP',
         name: 'Entertainer',
         description: `ESFP stands for Extroverted Sensing Feeling Perceiving. This personality type is known for being active, outgoing, happy-go-lucky, talkative, fun-loving, trustful and friendly. They very much appreciate and enjoy the tangible reality around them - they like to be physically active, to interact with other people and to have a lot of fun (music, art, shopping, partying, sports, are only some of their many interests). ESFPs are also loyal, affectionate and spirited people, they know how to tune into other people's emotional states and match their expectations and therefore they easily become very popular wherever they go. Born entertainers, they relish having an audience and being in the spotlight, but they can also become overly dramatic, demonstrative, frivolous and whimsical, especially when they are not getting the attention they desire.`,
-        images: [benSwolo, carSalesman, miniKeanuReeves, potionSeller, savagePatrick, stonks, surprisedPikachu, womanYellingAtCat]
+        images: [ESFP]
     }
 ];
